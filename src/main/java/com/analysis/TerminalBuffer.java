717cf9b3dc43158d1b898fd1f2905ad7e17ddf79
Teodor Andrei Vica <teodorvica@gmail.com>
2026-03-18 18:22:33 +0200

    Added functions for scroll and for PhysicalScreen coordonates

--- src/main/java/com/analysis/TerminalBuffer.java
+++ src/main/java/com/analysis/TerminalBuffer.java
@@ -83,4 +83,55 @@ public class TerminalBuffer {
     private int getPhysicalIndex(int row) {
         return (startPtr + row) % totalCapacity;
     }
+
+    public void writeText(String text) {
+        for (char c : text.toCharArray()) {
+            if (c == '\n') {
+                lineFeed();
+                continue;
+            }
+
+            // newline?
+            if (cursorCol >= width) {
+                lineFeed();
+            }
+
+            int physicalRow = getScreenRowPhysical(cursorRow);
+
+            // write in cell
+            buffer[physicalRow][cursorCol].reset(c, currentFg, currentBg, bold, italic, underline);
+            cursorCol++;
+        }
+    }
+
+    private void lineFeed() {
+        cursorCol = 0;
+        if (cursorRow < height - 1) {
+            cursorRow++;
+        } else {
+            // last line => Scroll up
+            // advance the start pointer to "remove" the top line
+            startPtr = (startPtr + 1) % totalCapacity;
+
+            // buffer not full => we can add more lines without losing old ones
+            if (totalLines < totalCapacity) {
+                totalLines++;
+            }
+
+            // cleaned the new bottom line
+            int newBottomRow = getScreenRowPhysical(height - 1);
+            for (int i = 0; i < width; i++) {
+                buffer[newBottomRow][i].reset(' ', 0, 0, false, false, false);
+            }
+        }
+    }
+
+    private int getScreenRowPhysical(int screenRow) {
+        // number of lines in scrollback (lines that are not currently visible on screen)
+        int scrollbackCount = Math.max(0, totalLines - height);
+        // logical Index of the line on the screen (0-based) + scrollbackCount = logical index in the buffer
+        return getPhysicalIndex(scrollbackCount + screenRow);
+    }
+
+
 }
